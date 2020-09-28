be77fad824e18d25f23fe3259d2813d60c769467
khurshidbekrumak <khurshidbekrumak@gmail.com>
2020-09-28 12:40:22 +0600

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -26,7 +26,7 @@ class Home extends Component {
 	render() {
 		return (
 			<div className="container2">
-				<div style={{fontSize: "14px", background: "white", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>
+				<!-- <div style={{fontSize: "14px", background: "white", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>
 					Source code: 
 					<IconButton style={{color: "black"}} onClick={() => window.location.href="https://github.com/0x5eba/Video-Meeting"}>
 						<GitHubIcon />
@@ -34,17 +34,17 @@ class Home extends Component {
 				</div>
 				
 				<div>
-					<h1 style={{ fontSize: "45px" }}>Video Meeting</h1>
-					<p style={{ fontWeight: "200" }}>Video conference website that lets you stay in touch with all your friends.</p>
-				</div>
+					<h1 style={{ fontSize: "45px" }}Sulpak Briefing</h1>
+					<p style={{ fontWeight: "200" }}>Для видеоконференций, который позволяет вам оставаться на связи со всеми своими коллегами.</p>
+				</div> -->
 
 				<div style={{
 					background: "white", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
 					textAlign: "center", margin: "auto", marginTop: "100px"
 				}}>
-					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Start or join a meeting</p>
-					<Input placeholder="URL" onChange={e => this.handleChange(e)} />
-					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>Go</Button>
+					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Начать встречу или присоединиться к ней</p>
+					<Input placeholder="Введи ссылку чтобы присоедениться" onChange={e => this.handleChange(e)} />
+					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>Начать</Button>
 				</div>
 			</div>
 		)
